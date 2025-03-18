d2f5e6286169509ed5616eccd461e13ad133ca34
AstroX11 <danielfx0012@gmail.com>
2025-03-18 17:22:05 +0100

    fixing for node.js support

--- environment.ts
+++ environment.ts
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const environment = {
-  SESSION: process.env.SESSION ?? '',
-  DATABASE_URL: process.env.DATABASE_URL ?? '',
-  META_DATA: process.env.META_DATA ?? '',
-  WARN_COUNT: process.env.WARN_COUNT ?? 3,
+  SESSION: process.env.SESSION ?? '', // Session id goes here
+  SESSION_URL: process.env.SESSION_URL ?? '', // Server of the custome session id
+  DATABASE_URL: process.env.DATABASE_URL ?? '', // Custome database path for sqlite db
+  META_DATA: process.env.META_DATA ?? '', // Metadata such as sticker and botname
+  WARN_COUNT: process.env.WARN_COUNT ?? 3, // Warn count before taking a decision
+  TIME_ZONE: process.env.TIME_ZONE ?? process.env.TZ, // Time zone of the bot
 };
--- src/utilities/session.mts
+++ src/utilities/session.mts
@@ -2,15 +2,14 @@ import { MakeSession } from '../index.mts';
 import { environment } from '../../environment.ts';
 
 const sessionId = environment.SESSION;
+const server = environment.SESSION_URL;
 
-export const getSession = async (
-  /** Your custom session server */
-  server?: string,
-): Promise<void | {
+export const getSession = async (): Promise<void | {
   creds: string;
   syncKeys: string;
 }> => {
   if (!sessionId) return console.log('No session provided');
+  if (!server) console.log('Session found, no custom server found!, using default...');
   const cipher = new MakeSession(
     sessionId,
     server ?? 'https://xstrosession.koyeb.app/session?session=',
