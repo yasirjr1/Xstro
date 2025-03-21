bed33d256cdd0bf4fd24c27ad7ddb6bd507d1c8b
AstroX11 <danielfx0012@gmail.com>
2025-03-21 18:27:06 +0100

    fix

--- environment.ts
+++ environment.ts
@@ -2,11 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const environment = {
-  DATABASE_URL: process.env.DATABASE_URL ?? '',
-  HUGGING_FACE_KEY: process.env.HUGGING_FACE_KEY ?? '',
-  META_DATA: process.env.META_DATA ?? 'AstroX11;Xstro',
   SESSION: process.env.SESSION ?? '',
   SESSION_URL: process.env.SESSION_URL ?? '',
+  HUGGING_FACE_KEY: process.env.HUGGING_FACE_KEY ?? '',
+  META_DATA: process.env.META_DATA ?? 'AstroX11;Xstro',
   TIME_ZONE: process.env.TIME_ZONE || process.env.TZ || '',
   WARN_COUNT: Number(process.env.WARN_COUNT ?? 3),
 };
