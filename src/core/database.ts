454cae49dae269343a2d69e30e2db1d7d41a2a65
AstroX11 <danielfx0012@gmail.com>
2025-04-03 20:38:40 +0100

    staging

--- src/core/database.ts
+++ src/core/database.ts
@@ -1,6 +1,13 @@
 import Database from '@astrox11/sqlite';
 import config from '../../config.js';
 
+/**
+ * Custom ORM handles everything effortlessly, and it supports only `node:sqlite`
+ * It can do everything Seqeulize does, but simpler, easier and very robust as well
+ * Libary is stable and can be found at https://github.com/AstroX11/sqlite
+ * This ORM is made to be promisfyable, for asynchrous tasks
+ * Be expecting a little overhead while using it
+ */
 const database = new Database(config.DATABASE, {
   open: true,
   enableForeignKeyConstraints: true,
@@ -8,3 +15,4 @@ const database = new Database(config.DATABASE, {
 });
 
 export default database;
+
