bc29082dc7a4c676e1da6aa89be1755d51aa5474
AstroX11 <danielfx0012@gmail.com>
2025-04-07 16:08:37 +0100

    fix

--- src/core/socket.ts
+++ src/core/socket.ts
@@ -1,9 +1,4 @@
-import {
-  makeWASocket,
-  useMultiFileAuthState,
-  makeCacheableSignalKeyStore,
-  Browsers,
-} from 'baileys';
+import { makeWASocket, makeCacheableSignalKeyStore, Browsers } from 'baileys';
 import config from '../../config.ts';
 import MakeListeners from '../api/makeEvents.ts';
 import { logger, connectProxy } from '../utils/index.ts';
@@ -12,8 +7,6 @@ import { useSqliteAuthStore } from '../functions/index.ts';
 
 export const initConnection = async () => {
   try {
-    logger.info('Starting connection...');
-    //  const { state, saveCreds } = await useMultiFileAuthState(config.SESSION_DIR || './session');
     const { state, saveCreds } = await useSqliteAuthStore();
     const sock = makeWASocket({
       auth: {
